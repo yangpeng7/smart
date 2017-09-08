2cebd52e6bf76e807efa0919eaf44b5216f5418a
yangpeng <826168051@qq.com>
2017-09-08 10:46:51 +0800

    【snow】initial commit

---
+++ src/main/java/com/democome/smart/SmartApplication.java
@@ -0,0 +1,12 @@
+package com.democome.smart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartApplication.class, args);
+	}
+}
---
+++ src/test/java/com/democome/smart/SmartApplicationTests.java
@@ -0,0 +1,16 @@
+package com.democome.smart;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmartApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
