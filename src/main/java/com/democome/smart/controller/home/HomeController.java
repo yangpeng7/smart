d617eff3def3b74d603dd32f052b7c58f6efe230
yangpeng <826168051@qq.com>
2017-09-19 17:56:04 +0800

    【snow】item test

--- src/main/java/com/democome/smart/controller/home/HomeController.java
+++ src/main/java/com/democome/smart/controller/home/HomeController.java
@@ -1,5 +1,10 @@
 package com.democome.smart.controller.home;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +14,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
 	@GetMapping(value = "/")
-	public String login() {
+	public String home(HttpServletRequest request) {
+
+		List<ProjectVO> projects = new ArrayList<>();
+		ProjectVO vo = new ProjectVO();
+		vo.setLanguage("JAVA");
+		vo.setName("测试");
+		projects.add(vo);
+		projects.add(vo);
+		projects.add(vo);
+		projects.add(vo);
+		projects.add(vo);
+		projects.add(vo);
+		projects.add(vo);
+		projects.add(vo);
+		projects.add(vo);
+		projects.add(vo);
+		
+		request.setAttribute("articles", projects);
+
 		return "home";
 	}
 
