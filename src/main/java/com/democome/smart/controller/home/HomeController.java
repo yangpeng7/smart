a3ff669229ca02f093fdd370fa96c835ef5c39e5
yangpeng <826168051@qq.com>
2017-09-20 20:06:12 +0800

    【snow】test pagination

--- src/main/java/com/democome/smart/controller/home/HomeController.java
+++ src/main/java/com/democome/smart/controller/home/HomeController.java
@@ -1,37 +1,43 @@
 package com.democome.smart.controller.home;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.democome.smart.model.filter.project.ProjectFilter;
+import com.democome.smart.model.project.Project;
+import com.democome.smart.service.project.ProjectService;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 
 @Controller
-@RequestMapping("/home")
 public class HomeController {
 
-	@GetMapping(value = "/")
-	public String home(HttpServletRequest request) {
+	@Autowired
+	private ProjectService service;
 
-		List<ProjectVO> projects = new ArrayList<>();
-		ProjectVO vo = new ProjectVO();
-		vo.setLanguage("JAVA");
-		vo.setName("测试");
-		projects.add(vo);
-		projects.add(vo);
-		projects.add(vo);
-		projects.add(vo);
-		projects.add(vo);
-		projects.add(vo);
-		projects.add(vo);
-		projects.add(vo);
-		projects.add(vo);
-		projects.add(vo);
-		
-		request.setAttribute("articles", projects);
+	@RequestMapping(value = "/home")
+	public String index(Model model, @RequestParam(defaultValue = "1") Integer pageNumber,
+			@RequestParam(defaultValue = "30") Integer pageSize) {
+
+		PageHelper.startPage(pageNumber, pageSize);
+
+		ProjectFilter filter = new ProjectFilter();
+
+		List<Project> projects = service.searchProjectByFilter(filter);
+
+		PageInfo<Project> pageInfo = new PageInfo<Project>(projects);
+
+		model.addAttribute("pageNum", pageInfo.getPageNum());
+		model.addAttribute("pageSize", pageInfo.getPageSize());
+		model.addAttribute("isFirstPage", pageInfo.isIsFirstPage());
+		model.addAttribute("totalPages", pageInfo.getPages());
+		model.addAttribute("isLastPage", pageInfo.isIsLastPage());
+		model.addAttribute("articles", pageInfo.getList());
 
 		return "home";
 	}
